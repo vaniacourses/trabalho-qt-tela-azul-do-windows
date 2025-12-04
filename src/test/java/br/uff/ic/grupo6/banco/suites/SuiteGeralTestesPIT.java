5e7f9dc4d13cb5f91b2139767335977c55a8f8d0
renatouff <renatoluiz@id.uff.br>
2025-12-03 22:35:20 -0300

    Suite para PITest
    Suite criada para rodar PITest em múltiplos pacotes.

---
+++ src/test/java/br/uff/ic/grupo6/banco/suites/SuiteGeralTestesPIT.java
@@ -0,0 +1,14 @@
+package br.uff.ic.grupo6.banco.suites;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+// Suite para rodar PIT Test em múltiplos pacotes do projeto
+@Suite
+@SelectPackages({
+    "br.uff.ic.grupo6.banco.estrutural", 
+    //"br.uff.ic.grupo6.banco.mutacao"
+})
+public class SuiteGeralTestesPIT {
+    // "conteiner" para rodar os pacotes acima juntos
+}
